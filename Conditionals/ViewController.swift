9790f4de270a782fce6f374572c06ce2c5e05d95
Patrick Stroyan <Patrick.stroyan2@gmail.com>
2022-02-10 13:19:21 -0700

    Initial Commit

---
+++ Conditionals/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Conditionals
+//
+//  Created by Patrick Stroyan on 2/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
